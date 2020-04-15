81f4b3b79b5fa43365ac839458666e1f9b7b1682
ranjith <ranjithreddyankilla@gmail.com>
2020-04-15 10:17:34 +0530

    done

--- w_app.py
+++ w_app.py
@@ -10,10 +10,11 @@ YOUR_APP_ID = "90768b2590dceed79369f286990fae0c"
 with open("country_info.json") as rf:
     contry_info = json.load(rf)
 for x in contry_info:
-    if x["name"].casefold() == c_name.casefold():
+    if x["name"].casefold() == c_name.casefold() or x['country'].casefold() == c_name.casefold():
         lat = x["latitude"]
         lon = x["longitude"]
         c_code = x["country"]
+        c_name = x['name']
         break
 if lat == None:
     print(f"Invalid input:{c_name}")
@@ -28,16 +29,22 @@ read_json = json.loads(json_data)
 
 
 def time_stamp(time):
+    return dt.datetime.fromtimestamp(time).time()
+
+
+def local_time(time):
     tz = pytz.timezone(read_json["timezone"])
-    return dt.datetime.fromtimestamp(time).now(tz)
+    return dt.datetime.strftime(
+        dt.datetime.fromtimestamp(time).now(tz), "%b %d, %Y %H:%M:%S %Z"
+    )
 
 
 timezone = read_json["timezone"]
-current_time = time_stamp(read_json["current"]["dt"])
+current_time = local_time(read_json["current"]["dt"])
 sunrise_time = time_stamp(read_json["current"]["sunrise"])
 sunset_time = time_stamp(read_json["current"]["sunset"])
 
-print(f"latitude:{lat},longitude:{lon}, country_code:{c_code}")
+print(f"latitude: {lat},longitude: {lon}, country_code: {c_code}, country_name: {c_name}")
 print(f"Timezone: {timezone}")
 print(f"Current Time: {current_time}")
 print(f"Sunrise Time: {sunrise_time}")
